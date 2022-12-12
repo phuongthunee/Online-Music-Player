ec3e985d7001ee4afa18832995217f193dc78f02
phuongthunee <lephuongthu18@gmail.com>
2022-12-12 19:12:15 +0700

    new fragment setup

--- app/src/main/java/vn/edu/usth/onlinemusicplayer/Register.java
+++ app/src/main/java/vn/edu/usth/onlinemusicplayer/Register.java
@@ -1,14 +1,28 @@
 package vn.edu.usth.onlinemusicplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import vn.edu.usth.onlinemusicplayer.fragments.SignInFragment;
 
 public class Register extends AppCompatActivity {
+
+    private FrameLayout frameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        frameLayout = findViewById(R.id.register);
+        setFragment(new SignInFragment());
+    }
+    private void setFragment(SignInFragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(frameLayout.getId(),fragment);
+        fragmentTransaction.commit();
     }
 }
