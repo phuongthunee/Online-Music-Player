7c7cf9fda5830fef6f0d6b11fb2b8c918e2fc9ca
hatrungt1n <hatrungtin2k2@gmail.com>
2022-12-15 18:28:33 +0700

    edit fragments

--- app/src/main/java/vn/edu/usth/onlinemusicplayer/SplashScreen.java
+++ app/src/main/java/vn/edu/usth/onlinemusicplayer/SplashScreen.java
@@ -1,9 +1,11 @@
 package vn.edu.usth.onlinemusicplayer;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.WindowManager;
@@ -12,20 +14,22 @@ import android.widget.ImageView;
 import vn.edu.usth.onlinemusicplayer.fragments.SignInFragment;
 
 public class SplashScreen extends AppCompatActivity {
-    private static int splash_screen = 3000;
-    ImageView image;
+    private Handler handler;
 
+    @RequiresApi(api = Build.VERSION_CODES.P)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        new Handler().postDelayed(new Runnable() {
+
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashScreen.this, SignInFragment.class);
+                Intent intent = new Intent(SplashScreen.this, Register.class);
                 startActivity(intent);
                 finish();
-            }
-        }, splash_screen);
+                }
+        }, 3000);
     }
 }
--- app/src/main/java/vn/edu/usth/onlinemusicplayer/fragments/SignInFragment.java
+++ app/src/main/java/vn/edu/usth/onlinemusicplayer/fragments/SignInFragment.java
@@ -1,5 +1,6 @@
 package vn.edu.usth.onlinemusicplayer.fragments;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -11,11 +12,17 @@ import android.view.ViewGroup;
 import vn.edu.usth.onlinemusicplayer.R;
 
 public class SignInFragment extends Fragment {
+    private TextView youDontAlreadyHaveAnAccount;
+    private TextView resetPassword;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_in, container, false);
+        View view = inflater.inflate(R.layout.fragment_sign_in, container, false);
+
+        youDontAlreadyHaveAnAccount= view.findViewById(R.id.youDontAlreadyHaveAnAccount);
+        resetPassword = view.findViewById(R.id.resetPassword);
+        return view;
     }
 }
