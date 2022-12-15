434176c214656671c6ab83658006a0ab16208d4d
phuongthunee <lephuongthu18@gmail.com>
2022-12-16 06:40:26 +0700

    fixed pixel and some fragments

--- app/src/main/java/vn/edu/usth/onlinemusicplayer/SplashScreen.java
+++ app/src/main/java/vn/edu/usth/onlinemusicplayer/SplashScreen.java
@@ -26,7 +26,7 @@ public class SplashScreen extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashScreen.this, SignInFragment.class);
+                Intent intent = new Intent(SplashScreen.this, Register.class);
                 startActivity(intent);
                 finish();
                 }
