6fd198116a91511da74c70b3b7eb744712370f62
phuongthunee <lephuongthu18@gmail.com>
2022-12-15 17:20:20 +0700

    edit splash screen

--- app/src/main/java/vn/edu/usth/onlinemusicplayer/MainActivity.java
+++ app/src/main/java/vn/edu/usth/onlinemusicplayer/MainActivity.java
@@ -2,13 +2,31 @@ package vn.edu.usth.onlinemusicplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import vn.edu.usth.onlinemusicplayer.fragments.SignInFragment;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static int splash_screen = 3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, SignInFragment.class);
+                startActivity(intent);
+                finish();
+            }
+        }, splash_screen);
     }
 }
--- app/src/main/java/vn/edu/usth/onlinemusicplayer/SplashScreen.java
+++ app/src/main/java/vn/edu/usth/onlinemusicplayer/SplashScreen.java
@@ -2,28 +2,30 @@ package vn.edu.usth.onlinemusicplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import vn.edu.usth.onlinemusicplayer.fragments.SignInFragment;
 
 public class SplashScreen extends AppCompatActivity {
-
-    private Handler handler;
-    private Runnable runnable;
+    private static int splash_screen = 3000;
+    ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-
-        handler = new Handler();
-        handler.postDelayed(new Runnable() {
+        new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashScreen.this, Register.class);
+                Intent intent = new Intent(SplashScreen.this, SignInFragment.class);
                 startActivity(intent);
                 finish();
             }
-        }, 3000);
+        }, splash_screen);
     }
 }
