9b3d9b9a3a3e8c43765dcdc5eda57e25fd7b1a27
phuongthunee <lephuongthu18@gmail.com>
2022-12-27 14:53:14 +0700

    fixed signin and main fragments

--- app/src/main/java/vn/edu/usth/onlinemusicplayer/MainActivity.java
+++ app/src/main/java/vn/edu/usth/onlinemusicplayer/MainActivity.java
@@ -1,22 +1,42 @@
 package vn.edu.usth.onlinemusicplayer;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import vn.edu.usth.onlinemusicplayer.fragments.SignInFragment;
+import vn.edu.usth.onlinemusicplayer.fragments.SignUpFragment;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView informations;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        informations = findViewById(R.id.informations);
+        informations.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Register.class);
+                startActivity(intent);
+            }
+        });
     }
 }
