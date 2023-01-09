d06899deac541a866afa0a6cc2e73cae4f62230d
phuongthunee <lephuongthu18@gmail.com>
2023-01-09 18:10:44 +0700

    ui

--- app/src/main/java/vn/edu/usth/onlinemusicplayer/MainActivity.java
+++ app/src/main/java/vn/edu/usth/onlinemusicplayer/MainActivity.java
@@ -1,28 +1,12 @@
 package vn.edu.usth.onlinemusicplayer;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.media.Image;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.WindowManager;
-import android.widget.FrameLayout;
-import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.Toast;
-
-import vn.edu.usth.onlinemusicplayer.fragments.SearchFragment;
-import vn.edu.usth.onlinemusicplayer.fragments.SignInFragment;
-import vn.edu.usth.onlinemusicplayer.fragments.SignUpFragment;
 
 public class MainActivity extends AppCompatActivity {
     private ImageView account;
--- app/src/main/java/vn/edu/usth/onlinemusicplayer/SplashScreen.java
+++ app/src/main/java/vn/edu/usth/onlinemusicplayer/SplashScreen.java
@@ -1,19 +1,10 @@
 package vn.edu.usth.onlinemusicplayer;
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.WindowManager;
-import android.widget.ImageView;
-
-import com.google.firebase.auth.FirebaseAuth;
-
-import vn.edu.usth.onlinemusicplayer.fragments.SignInFragment;
 
 public class SplashScreen extends AppCompatActivity {
     private Handler handler;
